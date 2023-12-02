87430f571fe64622adf416ccb30a194d6b64381e
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-12-02 16:50:47 +0700

    Clone UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        primary: "#030712"
+      },
     },
   },
   plugins: [],
